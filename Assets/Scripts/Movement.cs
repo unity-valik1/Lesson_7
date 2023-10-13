d7e7c288a32911a0b59b48bf595d112bac5a705b
unity-valik1 <unity_valik@mail.ru>
2023-10-14 02:53:33 +0300

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float gravity = -9.81f;
+    public float speed = 10f;
+
+    public CharacterController controller;
+
+    public Light flashlight;
+    private int flashlightActive = 0;
+
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource.Play();
+    }
+    private void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        float rotation = Input.GetAxis("Mouse X");
+
+
+        Vector3 movement = new Vector3(horizontal * speed, gravity, vertical * speed);
+
+        controller.Move(transform.TransformDirection(movement) * Time.deltaTime);
+        controller.transform.Rotate(Vector3.up, rotation);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if(flashlightActive == 0)
+            {
+                flashlight.gameObject.SetActive(true);
+                flashlightActive = 1;
+            }
+            else if(flashlightActive == 1)
+            {
+                flashlight.gameObject.SetActive(false);
+                flashlightActive = 0;
+            }
+        }
+        print(vertical);
+        if (vertical <= -0.2 || vertical >= 0.2 || horizontal <= -0.2 || horizontal >= 0.2)
+        {
+            audioSource.mute = false;
+        }
+        else
+        {
+            audioSource.mute = true;
+        }
+    }
+}
